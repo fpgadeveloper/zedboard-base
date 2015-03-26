84eacb622fc0f39d70525f11864169d97322cbee
Jeff Johnson <jeff@fpgadeveloper.com>
2015-03-26 15:29:28 -0400

    added files for Vivado 2014.4

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ To complete.
 
 ### Requirements
 
-* ISE Design Suite 14.7
+* ISE Design Suite 14.7 or Vivado 2014.4
 * ZedBoard
 
 ### License
---
+++ Vivado/src/bd/design_1.tcl
@@ -0,0 +1,68 @@
+################################################################
+# Check if script is running in correct Vivado version.
+################################################################
+set scripts_vivado_version 2014.4
+set current_vivado_version [version -short]
+
+if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+   puts ""
+   puts "ERROR: This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."
+
+   return 1
+}
+
+set design_name design_1
+
+# CHECKING IF PROJECT EXISTS
+if { [get_projects -quiet] eq "" } {
+   puts "ERROR: Please open or create a project!"
+   return 1
+}
+
+set cur_design [current_bd_design -quiet]
+set list_cells [get_bd_cells -quiet]
+
+create_bd_design $design_name
+
+current_bd_design $design_name
+
+set parentCell [get_bd_cells /]
+
+# Get object for parentCell
+set parentObj [get_bd_cells $parentCell]
+if { $parentObj == "" } {
+   puts "ERROR: Unable to find parent cell <$parentCell>!"
+   return
+}
+
+# Make sure parentObj is hier blk
+set parentType [get_property TYPE $parentObj]
+if { $parentType ne "hier" } {
+   puts "ERROR: Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."
+   return
+}
+
+# Save current instance; Restore later
+set oldCurInst [current_bd_instance .]
+
+# Set parent object as current
+current_bd_instance $parentObj
+
+# Add the Processor System and apply board preset
+startgroup
+create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
+endgroup
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
+
+# Configure the PS: Generate 100MHz clock, Enable GP0
+startgroup
+set_property -dict [list CONFIG.PCW_USE_M_AXI_GP0 {1}] [get_bd_cells processing_system7_0]
+endgroup
+
+# Connect the FCLK_CLK0 to the PS GP0
+connect_bd_net [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK]
+
+# Restore current instance
+current_bd_instance $oldCurInst
+
+save_bd_design
